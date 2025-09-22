5c14fc15d83bb9b9ffac27131e191e171fa6b4b8
Chandana-Malgireddy <malgireddychandana99@gmail.com>
2025-09-22 15:36:42 -0500

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Function to calculate sum of first 10 natural numbers
+int sumFirst10()
+{
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
